05d3aff3849f6e213ed4017e127ed50ae55d6084
enesbabaoglu <enessbabaoglu@gmail.com>
2021-03-22 15:22:23 +0300

    Update deploy.ps1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,6 +1,6 @@
 $Files = "C:\runner\_\dotnet-framework-actions-example\dotnet-framework-actions-example\GithubActionsExample\Example.API\outproject\_PublishedWebsites\Example.API\*"
 $Destination = "\\ZPMULWVCMSTG\c$\AppHosts\runnerApp"
-Start-Process powershell -Verb runAs copy-item -Path (Get-Item -Path "$Files").FullName -Include * -Destination "$Destination" -Force -Recurse -Confirm:$true
+copy-item -Path (Get-Item -Path "$Files").FullName -Include * -Destination "$Destination" -Force -Recurse -Confirm:$true
 
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
