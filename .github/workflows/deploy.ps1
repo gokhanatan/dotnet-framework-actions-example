531d0eb3980f80ef76101bc5fd0beb808d5c7ca5
enesbabaoglu <enessbabaoglu@gmail.com>
2021-03-22 15:39:59 +0300

    Update deploy.ps1

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,6 +1,4 @@
 $Files = "Microsoft.PowerShell.Core\FileSystem::\\ZPMULWVININENV\c$\runner\_\dotnet-framework-actions-example\dotnet-framework-actions-example\GithubActionsExample\Example.API\outproject\_PublishedWebsites\Example.API\*"
 $Destination = "Microsoft.PowerShell.Core\FileSystem::\\ZPMULWVCMSTG\c$\AppHosts\runnerApp"
-
-
-copy-item -Path (Get-Item -Path "$Files").FullName -Include * -Destination "$Destination" -Force -Recurse -Confirm:$true
+copy-item -Path (Get-Item -Path "$Files").FullName -Include * -Destination "$Destination" -Force -Recurse
 
